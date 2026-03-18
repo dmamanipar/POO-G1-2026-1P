4a8ec90317415a6124c52039daa59ce28d459b0a
jhanpol <jhanpool799@gmail.com>
2026-03-17 20:41:23 -0500

    Se hicieron cambios en la clase Jhanpol

---
+++ EjemploPOOG1/src/main/java/Jhanpol.java
@@ -0,0 +1,12 @@
+
+public class Jhanpol {
+    //jhanpol Brayan Quispe Lopez 202510867
+
+    public static void main(String[] args) {
+        saludarProfesor();
+    }
+
+    public static void saludarProfesor() {
+        System.out.println("Hola profesor, soy jhanpol");
+    }
+}
