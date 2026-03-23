51e1996dd9af94c46710dae2b3e1a5e414fabbc5
alexpehu <mamanipari@gmail.com>
2026-03-23 08:39:22 -0500

    Repaso trabajo con git

--- EjemploPOOG1/src/main/java/Ejemplo.java
+++ EjemploPOOG1/src/main/java/Ejemplo.java
@@ -1,6 +1,11 @@
 public class Ejemplo {
 
+
+    public static double suma(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         System.out.println("Hola Mundo del git");
+        System.out.println("Respuest Suma:"+suma(10,6));
     }
 }
---
+++ EjemploPOOG1/src/main/java/Persona.java
@@ -0,0 +1,4 @@
+public class Persona {
+    String dni;
+    String nombre;
+}
